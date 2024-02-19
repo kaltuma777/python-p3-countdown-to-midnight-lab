09f7c3de434c79108de7f5506c8e78e8e0653985
Kaltuma <Kaltuma.abdi@student.moringaschool.com>
2024-02-19 19:11:50 +0300

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,23 @@
 # your code goes here!
+import time
+
+def countdown(number):
+    x = int(number)
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+    print("CAN YOU BE MY VALENTINE!")
+
+# countdown(10)
+
+
+def countdown_with_sleep(number):
+    x = int(number)
+    while x > 0:
+        print(f"{x} SECOND(S)!")
+        x -= 1
+        time.sleep(1)
+
+    print("CAN YOU BE MY VALENTINE!")
+
+countdown_with_sleep(10)
--- lib/testing/countdown_test.py
+++ lib/testing/countdown_test.py
@@ -6,23 +6,23 @@ from countdown import countdown, countdown_with_sleep
 class TestCountdown:
     '''Function countdown() in countdown.py'''
 
-    def test_counts_down_prints_happy_new_year(self):
-        '''counts down from number and prints "HAPPY NEW YEAR!"'''
+    def test_counts_down_prints_CAN_YOU_BE_MY_VALENTINE(self):
+        '''counts down from number and prints "CAN YOU BE MY VALENTINE!"'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         countdown(5)
         sys.stdout = sys.__stdout__
         assert(captured_out.getvalue() == "5 SECOND(S)!\n4 SECOND(S)!\n" + \
-            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!\n")
+            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nCAN YOU BE MY VALENTINE!\n")
 
 class TestCountdownWithSleep:
     '''Function countdown_with_sleep() in countdown.py'''
 
     def test_counts_down_prints_happy_new_year(self):
-        '''counts down from number and prints "HAPPY NEW YEAR!"'''
+        '''counts down from number and prints "CAN YOU BE MY VALENTINE!"'''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         countdown_with_sleep(5)
         sys.stdout = sys.__stdout__
         assert(captured_out.getvalue() == "5 SECOND(S)!\n4 SECOND(S)!\n" + \
-            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nHAPPY NEW YEAR!\n")
+            "3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\nCAN YOU BE MY VALENTINE!\n")
